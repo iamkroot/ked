ea7c50cc8eb5b64fe6d4a67b6e308ef8c3a6d794
iamkroot <kroot.patel@gmail.com>
2023-02-12 15:28:00 +0530

    Add Ked struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::io::{self, Read};
+use std::io::{self, Read, StdinLock};
 use std::os::fd::RawFd;
 
 use log::error;
 use termios::{self, Termios};
 
 const STDIN_FD: RawFd = 0;
+
 fn enable_raw_mode() -> io::Result<()> {
     use termios::{
         BRKINT, CS8, ECHO, ICANON, ICRNL, IEXTEN, INPCK, ISIG, ISTRIP, IXON, OPOST, VMIN, VTIME,
@@ -21,13 +22,19 @@ fn enable_raw_mode() -> io::Result<()> {
     Ok(())
 }
 
-struct Cleanup {
+const fn ctrl_key(k: u8) -> u8 {
+    k & 0b0001_1111
+}
+
+struct Ked {
+    stdin: StdinLock<'static>,
     orig_termios: Termios,
 }
 
-impl Cleanup {
+impl Ked {
     fn new() -> io::Result<Self> {
-        Ok(Cleanup {
+        Ok(Ked {
+            stdin: std::io::stdin().lock(),
             orig_termios: Termios::from_fd(STDIN_FD)?,
         })
     }
@@ -36,9 +43,31 @@ impl Cleanup {
             error!("Failed to disable raw mode: {e}");
         });
     }
+    fn read_key(&mut self) -> io::Result<u8> {
+        let mut c: u8 = 0;
+        let buf = std::slice::from_mut(&mut c);
+        self.stdin.read(buf)?;
+        Ok(c)
+    }
+    fn process_key(&mut self) -> io::Result<()> {
+        let c = self.read_key()?;
+        log::trace!("Key {c}");
+        match c {
+            k if k == ctrl_key(b'q') => return Err(io::Error::new(io::ErrorKind::Other, "quit")),
+            _ => {
+                let ch: char = c.into();
+                if ch.is_ascii_control() {
+                    println!("{c}\r");
+                } else {
+                    println!("{c} ('{ch}')\r");
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
-impl Drop for Cleanup {
+impl Drop for Ked {
     fn drop(&mut self) {
         self.disable_raw_mode();
     }
@@ -50,27 +79,19 @@ fn main() -> io::Result<()> {
         .append(true)
         .open("ked.log")?;
     env_logger::builder()
-        .parse_default_env()
         .filter_level(log::LevelFilter::Debug)
+        .parse_default_env()
         .target(env_logger::Target::Pipe(Box::new(log_file)))
         .init();
-    let mut stdin = std::io::stdin().lock();
-    let cleanup = Cleanup::new()?;
+    // let mut stdin = ;
+    let mut ked = Ked::new()?;
     enable_raw_mode().expect("failed to enable raw");
-    let mut buf = [0; 1];
     loop {
-        buf[0] = 0;
-        stdin.read(&mut buf)?;
-        let c = char::from(buf[0]);
-        if c.is_ascii_control() {
-            println!("{}\r", buf[0]);
-        } else {
-            println!("{} ('{c}')\r", buf[0]);
-        }
-        if c == 'q' {
+        if let Err(e) = ked.process_key() {
+            log::error!("Error! {e}");
             break;
         }
     }
-    drop(cleanup);
+    drop(ked);
     Ok(())
 }
